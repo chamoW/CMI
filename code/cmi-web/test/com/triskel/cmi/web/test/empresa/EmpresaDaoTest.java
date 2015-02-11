99c413f4d1527246e9539f69936e0caf18641ff2
wwlopez <wwlopez@PC01TEC212.cobiscorp.int>
2015-02-11 16:15:42 -0500

    se configura para eclipselink provider, best jpa provider

---
+++ code/cmi-web/test/com/triskel/cmi/web/test/empresa/EmpresaDaoTest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.triskel.cmi.web.test.empresa;
+
+
+import com.triskel.cmi.ejb.entities.Empresa;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author wwlopez
+ */
+public class EmpresaDaoTest extends GenericoTest{
+    
+    @Test
+    public void contarTest(){
+        Integer numeroDatos = empresaDao.contarDatos();
+        logger.error("numeroDatos: " + numeroDatos);        
+        
+        Assert.assertNotSame(1, numeroDatos);                
+    }
+    
+    
+    @Test
+    public void buscarTodoTest(){
+        List<Empresa> lista = empresaDao.searchAll();
+        
+        for(Empresa tmp : lista){
+            logger.error("tmp.getNombre(): " + tmp.getNombre());        
+        }                
+    }
+    
+}
